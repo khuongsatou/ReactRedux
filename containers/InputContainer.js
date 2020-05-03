747acb72369634cc53511a771e3d7d87c1d12350
nvkhuong <vankhuong240499@gmail.com>
2020-05-03 11:06:04 +0700

    Update useSelector and use Dispatch

---
+++ containers/InputContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, SafeAreaView} from 'react-native';
+import InputComponent from '../components/InputComponent';
+
+export default function InputComponent() {
+  return (
+    <View>
+      <InputComponent />
+    </View>
+  );
+}
